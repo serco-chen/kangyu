9c455a5209cd15e3cedb772821995ba2a1d4cbc0
serco-chen <xg.chen87@gmail.com>
2015-04-05 16:49:13 +0800

    modify Gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.3.5'
 
 set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:serco-chen/kangyu.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
